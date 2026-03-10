ce735643180b4d453a438b03952d3c25fc8347b1
汉堡包 <syahirfan545@gmail.com>
2026-03-10 10:43:02 +0700

    add variable declarations for rectangle dimensions

---
+++ function_with_parameter.cpp
@@ -0,0 +1,5 @@
+#include <iostream> // untuk input dan output
+using namespace std; // deklarasi variabel global
+
+int panjang, lebar; // fungsi untuk menghitung luas dan keliling persegi panjang
+
